93bbd2eddb0e486bca6232fc16821679f5872043
Alberto Casa <alexo2407@gmail.com>
2025-02-13 23:23:21 -0600

    Api crea pedido

--- api/auth/login.php
+++ api/auth/login.php
@@ -2,15 +2,12 @@
 require_once __DIR__ . '/../utils/auth.php';
 require_once __DIR__ . '/../utils/responder.php';
 
-ini_set('display_errors', 1);
+/*ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ALL);*/
 
 // Encabezados para CORS
-header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: POST');
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Authorization');
 
 // Verificar si el método es POST
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
