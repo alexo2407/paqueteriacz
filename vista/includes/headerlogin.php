503985b4ec8164e5b66b3b5b6237587041e36bef
Alberto <alexo2407@gmail.com>
2025-02-14 08:50:25 -0600

    agregando el login front end

---
+++ vista/includes/headerlogin.php
@@ -0,0 +1,12 @@
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="<?=RUTA_URL?>">Paqueteria CruzValle</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+       
+    </div>
+</nav>
+
+<div class="container mt-5 caja">
--- vista/modulos/inicio.php
+++ vista/modulos/inicio.php
@@ -1,6 +1,33 @@
-<?php include "vista/includes/header.php"; ?>
+<?php include "vista/includes/headerlogin.php"; ?>
 
+<div class="container login">
 
-<h1>hola</h1>
+<div class="login-card text-center">
+    <i class="fas fa-user-circle mb-3"></i>
+    <h3 class="mb-4">Inicio de Sesión</h3>
+    <form>
+        <div class="mb-3">
+            <div class="input-group">
+                <span class="input-group-text bg-white border-end-0">
+                    <i class="fas fa-envelope"></i>
+                </span>
+                <input type="email" class="form-control border-start-0" placeholder="Correo Electrónico" required>
+            </div>
+        </div>
+        <div class="mb-3">
+            <div class="input-group">
+                <span class="input-group-text bg-white border-end-0">
+                    <i class="fas fa-lock"></i>
+                </span>
+                <input type="password" class="form-control border-start-0" placeholder="Contraseña" required>
+            </div>
+        </div>
+        <button type="submit" class="btn btn-login w-100">Iniciar Sesión</button>
+    </form>
+    <div class="mt-3">
+        <small><a href="#" class="text-decoration-none">¿Olvidaste tu contraseña?</a></small>
+    </div>
+</div>
+</div>
 
 <?php include "vista/includes/footer.php" ?>
