d131b52d026f7acc2ef45a5ac1f207fb3b3a53d6
Alberto Casa <alexo2407@gmail.com>
2025-02-13 23:31:03 -0600

    Api crea pedido

--- api/pedidos/crear.php
+++ api/pedidos/crear.php
@@ -1,9 +1,9 @@
 <?php
- 
+ /*
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL); 
-
+*/
 // Encabezados para CORS
   header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
@@ -13,9 +13,6 @@ header('Content-Type: application/json');
 require_once __DIR__ . '/../utils/autenticacion.php';
 require_once __DIR__ . '/../utils/responder.php';
 require_once __DIR__ . '/../../controlador/pedido.php';
-
-
-header('Content-Type: application/json');
 
 
 // Obtener encabezados de la solicitud
