8b8b7ec4260d5e18ed54f8819a189cc653087ab0
Alberto <alexo2407@gmail.com>
2025-10-22 11:21:29 -0600

    Agregar sección de proveedores en el menú y crear archivos listar.php y editar.php para gestionar proveedores

--- vista/includes/header.php
+++ vista/includes/header.php
@@ -31,6 +31,9 @@
                     <a class="nav-link" href="<?= RUTA_URL ?>productos/listars">productos</a>
                 </li>
                 <li class="nav-item">
+                    <a class="nav-link" href="<?= RUTA_URL ?>productos/listars">prooveedor</a>
+                </li>
+                <li class="nav-item">
                     <a class="nav-link" href="<?= RUTA_URL ?>seguimientos/listar">seguimiento</a>
                 </li>
 
