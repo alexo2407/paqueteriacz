7ecbfaf51d52443f89757a50d5e3c6da4bbdb510
Alberto Casa <alexo2407@gmail.com>
2026-02-25 13:44:41 -0600

    chore: elimina scripts temporales de inspeccion de BD

--- _est.php
+++
@@ -1,4 +0,0 @@
-<?php
-require 'config/config.php'; require 'modelo/conexion.php';
-$db=(new Conexion())->conectar();
-print_r($db->query('DESCRIBE estados_pedidos')->fetchAll(PDO::FETCH_ASSOC));
--- _routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'config/config.php'; require 'modelo/conexion.php';
-$db=(new Conexion())->conectar();
-// Buscar rutas de stock por enlace
-$rows = $db->query("SELECT * FROM enlaces WHERE enlace LIKE '%stock%' ORDER BY id")->fetchAll(PDO::FETCH_ASSOC);
-print_r($rows);
